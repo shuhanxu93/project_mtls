8cc244363d15f6486f34a3a28bee66167136853f
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-02-20 21:44:50 +0100

    tested predictor with training set and updated journal

--- parser.py
+++ parser.py
@@ -1,7 +1,5 @@
 import numpy as np
 from sklearn import svm
-from sklearn.preprocessing import LabelBinarizer
-np.set_printoptions(threshold=np.nan)
 
 def parser(filename):
     '''Parse though a protein sequence file and return lists of headers, sequences and structures'''
@@ -22,11 +20,11 @@ def parser(filename):
     return headers, sequences, structures
 
 def create_dataset(raw_sequences, window_size):
-    """Create a matrix of n_samples * n_features. n_features = 21 amino acids * window_size."""
+    """Create a matrix of n_samples * n_features. n_features = amino_size * window_size."""
 
     # append Xs to heads and tails of sequences
     half_window = window_size // 2
-    adjusted_sequences = ['X'*half_window + i + 'X'*half_window for i in raw_sequences]
+    adjusted_sequences = ['J'*half_window + i + 'J'*half_window for i in raw_sequences]
 
     # breakdown sequences into window-sized pieces
     fragmented_sequences = []
@@ -36,15 +34,16 @@ def create_dataset(raw_sequences, window_size):
             fragmented_sequences.append(seq[j-half_window:j+half_window+1])
 
     # Create amino acid converter
-    amino_acids = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'X']
-    amino_num = [i for i in range(len(amino_acids))]
+    amino_acids = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'J', 'X', 'B', 'Z']
+    amino_size = len(amino_acids)
+    amino_num = [i for i in range(amino_size)]
     amino2num = dict((x, y) for x, y in zip(amino_acids, amino_num))
 
     # convert sequences to array of binaries
-    data_array = np.zeros((len(fragmented_sequences), 21 * window_size), dtype=int)
+    data_array = np.zeros((len(fragmented_sequences), amino_size * window_size), dtype=int)
     for i in range(len(fragmented_sequences)):
         for j in range(len(fragmented_sequences[i])):
-            data_array[i, j * 21 + amino2num[fragmented_sequences[i][j]]] = 1
+            data_array[i, j * amino_size + amino2num[fragmented_sequences[i][j]]] = 1
 
     return data_array
 
@@ -54,6 +53,19 @@ def create_groundtruth(raw_structures):
     structure_array = np.array(list(long_string))
     return structure_array
 
-headers, sequences, structures = parser('test_set.txt')
+headers, sequences, structures = parser('cas3.3line.txt')
 
-print(create_dataset(sequences, 7))
+
+questions = create_dataset(sequences, 17)[:11000]
+answers = create_groundtruth(structures)[:11000]
+
+print("preprocessing done")
+
+clf = svm.SVC(C=1000)
+clf.fit(questions, answers)
+
+predictions = clf.predict(questions)
+
+accuracy = np.mean(predictions == answers)
+
+print(accuracy)
