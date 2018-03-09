c6271e36515654495a2bc05173f0cf28351019aa
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-03-09 17:16:19 +0100

    add bash script

---
+++ bash/psi_blast.sh
@@ -0,0 +1,3 @@
+cd ../datasets/fasta
+for file in *.fasta
+do psiblast -query $file -evalue 0.01 -db ../../../my_database/uniprot_sprot.fasta -num_iterations 3 -out ../psiblast/$file.psiblast -out_ascii_pssm ../pssm/$file.pssm -num_threads 8
