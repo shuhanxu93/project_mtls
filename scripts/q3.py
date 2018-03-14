0c3e03fc6f075a171d9610b2688f2921e7f0574d
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-03-14 16:44:26 +0100

    almost finished script for scoring

---
+++ scripts/q3.py
@@ -0,0 +1,135 @@
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+
+
+np.set_printoptions(threshold=np.nan)
+
+
+def main(dataset_file, window_size, model_file):
+
+    # parsing file
+    ids, seq, sec = parse(dataset_file)
+
+    # shuffle(random_state=0) and split sequences and structures into training(70%) and test sets(30%)
+    X_train, X_test, y_train, y_test = train_test_split(seq, sec, test_size=0.3, random_state=0)
+
+    clf = joblib.load(model_file)
+
+    q3_scores = np.zeros(len(X_test))
+    for index in range(len(X_test)):
+        sequence = [X_test[index]]
+        structure = [y_test[index]]
+        sequence_fragmented, dummy = fragment(sequence, window_size)
+        sequence_encoded = encode_attributes(sequence_fragmented)
+        structure_encoded = encode_targets(structure)
+        q3_scores[index] = clf.score(sequence_encoded, structure_encoded)
+    q3_ave = np.mean(q3_scores)
+
+
+
+def parse(filename):
+    """Parse though a protein sequence and secondary structure file
+       and return lists of headers, sequences and structures"""
+    headers = []
+    sequences = []
+    structures = []
+
+    with open(filename) as fh:
+        while True:
+            header = fh.readline().rstrip()[1:]
+            sequence = fh.readline().rstrip()
+            structure = fh.readline().rstrip()
+            if len(structure) == 0:
+                break
+            headers.append(header)
+            sequences.append(sequence)
+            structures.append(structure)
+    return headers, sequences, structures
+
+
+def fragment(sequences, window_size):
+    """Take a list of protein sequences and return a list of window-sized sequences"""
+
+    half_window = window_size // 2
+
+    fragmented_sequences = []
+    groups = []
+    for seq_idx, seq in enumerate(sequences):
+        for amino_idx in range(len(seq)):
+            if amino_idx < half_window:
+                # N-terminal cases
+                fragmented_sequences.append('0' * (half_window - amino_idx) + seq[:amino_idx + half_window + 1])
+                groups.append(seq_idx)
+            elif amino_idx >= len(seq) - half_window:
+                # C-terminal cases
+                fragmented_sequences.append(seq[amino_idx - half_window:] + '0' * (half_window - len(seq) + amino_idx + 1))
+                groups.append(seq_idx)
+            else:
+                fragmented_sequences.append(seq[amino_idx - half_window:amino_idx + half_window + 1])
+                groups.append(seq_idx)
+    return fragmented_sequences, groups
+
+
+def encode_attributes(fragmented_sequences):
+    """Take a list of fragmented sequences and return a numpy array of one-hot encodings"""
+
+    window_size = len(fragmented_sequences[0])
+
+    encoded_sequences = np.zeros((len(fragmented_sequences), window_size, 20))
+    for i in range(len(fragmented_sequences)):
+        for j in range(window_size):
+            encoded_sequences[i, j] = amino_code[fragmented_sequences[i][j]]
+    encoded_sequences = encoded_sequences.reshape(len(fragmented_sequences), window_size * 20)
+
+    return encoded_sequences
+
+
+def encode_targets(structures):
+    """Take a list of secondary structures and return a numpy array of class labels"""
+
+    structures_str = ''.join(structures).translate(trans_table)
+    encoded_structures = np.array(list(structures_str), dtype=int)
+    return encoded_structures
+
+# Create amino acid converter using dictionary
+amino_code = {'A': np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'R': np.array([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'N': np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'D': np.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'C': np.array([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'Q': np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'E': np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'G': np.array([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'H': np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'I': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'L': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'K': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'M': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]),
+              'F': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]),
+              'P': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]),
+              'S': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]),
+              'T': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]),
+              'W': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]),
+              'Y': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]),
+              'V': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
+              'X': np.array([0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05]),
+              'B': np.array([0, 0, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'Z': np.array([0, 0, 0, 0, 0, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              'J': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+              '0': np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])}
+
+
+# Create secondary structure converter using str.maketrans()
+trans_table = str.maketrans("HEC", "012")
+structure_name = np.array(['H', 'E', 'C'])
+
+
+if __name__ == '__main__':
+    main('../datasets/cas3.3line.txt', 15, '../models/test_balanced.pkl') # modify window size and model_file here (last 2 arguments)
