ce8df079c8be5939eac9e3f9eeeb9b69ce580204
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-03-15 23:17:07 +0100

    modified my dssp to fasta script

--- scripts/dssp_to_fasta.py
+++ scripts/dssp_to_fasta.py
@@ -1,20 +1,27 @@
 import numpy as np
+from re import sub
 
 fh = open("../datasets/pdb_ids.txt", 'r')
-
 pdb_ids = fh.read().splitlines()
-
 fh.close()
 
-seq = []
-
-sec = []
-
-number = 0
+ids = []
+sequences = []
+structures = []
 for pdb_id in pdb_ids:
     dssp_filename = '../datasets/dssp/pdb' + pdb_id.lower() + '.ent.dssp'
     check = np.genfromtxt(dssp_filename, skip_header=28, usecols=1)
-    print(pdb_id)
-    if not np.isnan(check).any():
-        seq.append(pdb_id)
-print(seq)
+    if not np.isnan(check).any(): # check if structure is one single sequence
+        ids.append(pdb_id)
+        seq = ''.join(np.genfromtxt(dssp_filename, skip_header=28, usecols=1,
+                      dtype=str, delimiter=[13,1]).tolist())
+        seq = sub("[a-z]", 'C', seq) # convert lowercase amino acid to cysteine
+        sequences.append(seq)
+        sec = ''.join(np.genfromtxt(dssp_filename, skip_header=28, usecols=1,
+                      dtype=str, delimiter=[16,1]).tolist())
+        sec = sec.replace(' ', 'L') # convert space to 'L'
+        structures.append(sec)
+
+print(len(ids))
+print(len(sequences))
+print(len(structures))
