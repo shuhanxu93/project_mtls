46a568a48109d7907b8dfecef64d484f613c2767
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-03-18 10:20:55 +0100

    finished random forest

--- scripts/plot_fm_line.py
+++ scripts/plot_fm_line.py
@@ -9,13 +9,13 @@ y2_mean = []
 y2_std = []
 
 for window_size in x:
-    filename = '../results/fm_linear/fm_linear_none_' + str(window_size) + '.csv' # change filename here
+    filename = '../results/fm_dt/fm_dt_none_' + str(window_size) + '.csv' # change filename here
     df = pd.read_csv(filename, sep='\t', encoding='utf-8')
     y1_mean.append(df['mean_test_score'].values.max())
     y1_std.append(df['std_test_score'].values[df['mean_test_score'].values.argmax()])
 
 for window_size in x:
-    filename = '../results/fm_linear/fm_linear_balanced_' + str(window_size) + '.csv' # change filename here
+    filename = '../results/fm_dt/fm_dt_balanced_' + str(window_size) + '.csv' # change filename here
     df = pd.read_csv(filename, sep='\t', encoding='utf-8')
     y2_mean.append(df['mean_test_score'].values.max())
     y2_std.append(df['std_test_score'].values[df['mean_test_score'].values.argmax()])
@@ -25,12 +25,12 @@ fig, ax = plt.subplots()
 line1 = ax.errorbar(x, y1_mean, yerr=y1_std, capsize=4, label='class_weight=None')
 line2 = ax.errorbar(x, y2_mean, yerr=y2_std, capsize=4, label='class_weight=\'balanced\'')
 ax.set_xticks(x)
-ax.set_ylim(0.65, 0.8)
-ax.set_title("Five-fold Cross Validation Accuracy using Optimal Hyper-parameters, LinearSVC()", fontsize=10)
+ax.set_ylim(0.40, 0.8)
+ax.set_title("Five-fold Cross Validation Accuracy using Optimal Hyper-parameters, DecisionTreeClassifier()", fontsize=9)
 ax.set_xlabel("Window Size")
 ax.set_ylabel("Accuracy")
 ax.legend(loc='lower right')
 print(y1_mean)
 print(y2_mean)
 #plt.show()
-plt.savefig('../report_figures/line_fm_linear.png')
+plt.savefig('../report_figures/line_fm_dt.png')
--- scripts/plot_seq_line.py
+++ scripts/plot_seq_line.py
@@ -9,13 +9,13 @@ y2_mean = []
 y2_std = []
 
 for window_size in x:
-    filename = '../results/seq_linear/seq_linear_none_' + str(window_size) + '.csv' # change filename here
+    filename = '../results/seq_rf/seq_rf_none_' + str(window_size) + '.csv' # change filename here
     df = pd.read_csv(filename, sep='\t', encoding='utf-8')
     y1_mean.append(df['mean_test_score'].values.max())
     y1_std.append(df['std_test_score'].values[df['mean_test_score'].values.argmax()])
 
 for window_size in x:
-    filename = '../results/seq_linear/seq_linear_balanced_' + str(window_size) + '.csv' # change filename here
+    filename = '../results/seq_rf/seq_rf_balanced_' + str(window_size) + '.csv' # change filename here
     df = pd.read_csv(filename, sep='\t', encoding='utf-8')
     y2_mean.append(df['mean_test_score'].values.max())
     y2_std.append(df['std_test_score'].values[df['mean_test_score'].values.argmax()])
@@ -25,12 +25,12 @@ fig, ax = plt.subplots()
 line1 = ax.errorbar(x, y1_mean, yerr=y1_std, capsize=4, label='class_weight=None')
 line2 = ax.errorbar(x, y2_mean, yerr=y2_std, capsize=4, label='class_weight=\'balanced\'')
 ax.set_xticks(x)
-ax.set_ylim(0.55, 0.7)
-ax.set_title("Five-fold Cross Validation Accuracy using Optimal Hyper-parameters, LinearSVC()", fontsize=10)
+ax.set_ylim(0.40, 0.70)
+ax.set_title("Five-fold Cross Validation Accuracy using Optimal Hyper-parameters, RandomForestClassifier()", fontsize=9)
 ax.set_xlabel("Window Size")
 ax.set_ylabel("Accuracy")
 ax.legend(loc='lower right')
 print(y1_mean)
 print(y2_mean)
 #plt.show()
-plt.savefig('../report_figures/line_seq_linear.png')
+plt.savefig('../report_figures/line_seq_rf.png')
